831f1b084809427b321f71d53c5b6ce3c77b1723
Karen Feng <karenfeng.us@gmail.com>
2017-02-05 05:13:01 -0500

    Attempts to speed up Caffe

--- examples/imagenet/train_caffenet.sh
+++ examples/imagenet/train_caffenet.sh
@@ -4,4 +4,4 @@ set -e
 CAFFE_HOME_DIR=~/Desktop/caffe
 TOOLS=$CAFFE_HOME_DIR/build/tools
 $TOOLS/caffe train \
-    --solver=$CAFFE_HOME_DIR/models/bvlc_reference_caffenet/solver.prototxt $@
+    --solver=$CAFFE_HOME_DIR/models/bvlc_reference_caffenet/solver.prototxt -iterations 3 $@
