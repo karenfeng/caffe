cbdef2815dc04f2ffe3a9a5e51e13035366a612d
Karen Feng <karenfeng.us@gmail.com>
2017-02-05 03:12:06 -0500

    Progress toward getting caffe working

--- examples/imagenet/make_imagenet_mean.sh
+++ examples/imagenet/make_imagenet_mean.sh
@@ -2,9 +2,10 @@
 # Compute the mean image from the imagenet training lmdb
 # N.B. this is available in data/ilsvrc12
 
-EXAMPLE=examples/imagenet
-DATA=data/ilsvrc12
-TOOLS=build/tools
+CAFFE_HOME_DIR=~/Desktop/caffe
+EXAMPLE=$CAFFE_HOME_DIR/examples/imagenet
+DATA=$EXAMPLE/labels
+TOOLS=$CAFFE_HOME_DIR/build/tools
 
 $TOOLS/compute_image_mean $EXAMPLE/ilsvrc12_train_lmdb \
   $DATA/imagenet_mean.binaryproto
--- examples/imagenet/train_caffenet.sh
+++ examples/imagenet/train_caffenet.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 set -e
 
-./build/tools/caffe train \
+CAFFE_HOME_DIR=~/Desktop/caffe
+TOOLS=$CAFFE_HOME_DIR/build/tools
+$TOOLS/caffe train \
     --solver=models/bvlc_reference_caffenet/solver.prototxt $@
